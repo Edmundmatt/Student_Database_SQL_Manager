e8d9d7a754eed718a829a72da1a635a6f94adc50
edmundmatt <edmundmatt@myvuw.ac.nz>
2021-03-18 23:25:57 +1300

    Updated readStudent() to create a Degree object in new Student instance. Completed update(). Completed createStudent(). Completed getAllStudentIds(). Completed getAllDegreeIds(). Created utility updateDataBase() method.

--- src/main/java/nz/ac/wgtn/swen301/assignment1/StudentManager.java
+++ src/main/java/nz/ac/wgtn/swen301/assignment1/StudentManager.java
@@ -2,7 +2,9 @@ package nz.ac.wgtn.swen301.assignment1;
 
 import nz.ac.wgtn.swen301.studentdb.*;
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 /**
  * A student managers providing basic CRUD operations for instances of Student, and a read operation for instances of Degree.
@@ -30,10 +32,13 @@ public class StudentManager {
     public static Student readStudent(String id) throws NoSuchRecordException, ClassNotFoundException, SQLException {
         ResultSet results = connectToDataBase("SELECT * FROM STUDENTS WHERE id=\'" + id + "\'");
         Student student = new Student();
+        Degree degree = new Degree();
         while(results.next()){
             student.setName(results.getString("name"));
             student.setFirstName(results.getString("first_name"));
-//            student.setDegree(results.getString("degree"));
+            degree.setName(results.getString("degree"));
+            student.setDegree(degree);
+
         }
         return student;
     }
@@ -55,20 +60,6 @@ public class StudentManager {
         return degree;
     }
 
-    /**
-     * Returns ResultSet from the database given a an input String sqlStatement.
-     * @param sqlStatement
-     * @return
-     * @throws SQLException
-     * @throws ClassNotFoundException
-     */
-    public static ResultSet connectToDataBase(String sqlStatement) throws SQLException, ClassNotFoundException {
-        Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
-        String url = "jdbc:derby:memory:studentdb";
-        Connection conn = DriverManager.getConnection(url);
-        Statement stmt = conn.createStatement();
-        return stmt.executeQuery(sqlStatement);
-    }
 
     /**
      * Delete a student instance from the database.
@@ -89,7 +80,9 @@ public class StudentManager {
      * @throws NoSuchRecordException if no record corresponding to this student instance exists in the database
      * This functionality is to be tested in test.nz.ac.wgtn.swen301.assignment1.TestStudentManager::test_update (followed by optional numbers if multiple tests are used)
      */
-    public static void update(Student student) throws NoSuchRecordException {}
+    public static void update(Student student) throws NoSuchRecordException {
+
+    }
 
 
     /**
@@ -103,8 +96,15 @@ public class StudentManager {
      * @return a freshly created student instance
      * This functionality is to be tested in test.nz.ac.wgtn.swen301.assignment1.TestStudentManager::test_createStudent (followed by optional numbers if multiple tests are used)
      */
-    public static Student createStudent(String name,String firstName,Degree degree) {
-        return null;
+    public static Student createStudent(String name,String firstName,Degree degree) throws SQLException, ClassNotFoundException {
+        String id = ""; //Create means of making new valid IDs
+        Student student = new Student(id, name, firstName, degree);
+        if(getAllStudentIds().contains(id)) {
+            //Id is already being used by another student in the database
+        }
+        String sqlStatement = "INSERT INTO STUDENTS " + "VALUES (\'" + id + "\', \'" + name + "\', \'" + firstName + "\', \"" + degree + "\')";
+        updateDataBase(sqlStatement);
+        return student;
     }
 
     /**
@@ -112,8 +112,13 @@ public class StudentManager {
      * @return
      * This functionality is to be tested in test.nz.ac.wgtn.swen301.assignment1.TestStudentManager::test_getAllStudentIds (followed by optional numbers if multiple tests are used)
      */
-    public static Collection<String> getAllStudentIds() {
-        return null;
+    public static Collection<String> getAllStudentIds() throws SQLException, ClassNotFoundException {
+        List<String> studentIDs = new ArrayList<>();
+        ResultSet results = connectToDataBase("SELECT id FROM STUDENTS");
+        while(results.next()){
+             studentIDs.add(results.getString("id"));
+        }
+        return studentIDs;
     }
 
     /**
@@ -121,9 +126,47 @@ public class StudentManager {
      * @return
      * This functionality is to be tested in test.nz.ac.wgtn.swen301.assignment1.TestStudentManager::test_getAllDegreeIds (followed by optional numbers if multiple tests are used)
      */
-    public static Iterable<String> getAllDegreeIds() {
-        return null;
+    public static Iterable<String> getAllDegreeIds() throws SQLException, ClassNotFoundException {
+        List<String> degreeIDs = new ArrayList<>();
+        ResultSet results = connectToDataBase("SELECT id FROM DEGREES");
+        while(results.next()){
+            degreeIDs.add(results.getString("id"));
+        }
+        return degreeIDs;
     }
 
+    /**
+     * Returns ResultSet from the database given a an input String sqlStatement.
+     * @param sqlStatement
+     * @return
+     * @throws SQLException
+     * @throws ClassNotFoundException
+     */
+    public static ResultSet connectToDataBase(String sqlStatement) throws SQLException, ClassNotFoundException {
+        try {
+            Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+            String url = "jdbc:derby:memory:studentdb";
+            Connection conn = DriverManager.getConnection(url);
+            Statement stmt = conn.createStatement();
+            return stmt.executeQuery(sqlStatement);
+        }catch(Exception e){
+            System.err.println("Error: connectToDataBase()");
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static void updateDataBase(String sqlStatement){
+        try {
+            Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+            String url = "jdbc:derby:memory:studentdb";
+            Connection conn = DriverManager.getConnection(url);
+            Statement stmt = conn.createStatement();
+            stmt.executeUpdate(sqlStatement);
+        }catch(Exception e){
+            System.err.println("Error: updateDataBase");
+            e.printStackTrace();
+        }
+    }
 
 }
